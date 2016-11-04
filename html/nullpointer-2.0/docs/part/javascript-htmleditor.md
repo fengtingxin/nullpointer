b8b474c108595c93864829ebb37bf413f4916f36
zhangzhaolin <626344962@qq.com>
2016-11-04 16:27:45 +0800

    更新网站版本：增加ZUI框架

---
+++ html/nullpointer-2.0/docs/part/javascript-htmleditor.md
@@ -0,0 +1,136 @@
+﻿section: javascript
+id: kindeditor
+description: 使用Kineditor来编辑富文本
+icon: icon-edit
+filter: fuwenbenbianjiqi fwbbjq
+---
+
+# 富文本编辑器
+
+ZUI 推荐两种富文本编辑器解决方案：UEditor 和 Kindeitor。
+
+ZUI 专为 UEditor 和 Kindeitor 定制了与 ZUI 风格一致的主题。
+
+## UEditor
+
+<div class="alert alert-danger">
+  <h4>兼容性问题</h4>
+  <p>在触摸屏或小屏幕上无法获取最佳体验。</p>
+</div>
+
+### 使用 UEditor
+
+因为 UEditor 用到的文件比较多，ZUI 并不提供 UEditor JavaScript 文件。你需要访问 UEditor 官方网站来下载相关资源，以下为 UEditor 相关资源网站：
+
+ - 官方定制：<a target="_blank" href="http://ueditor.baidu.com/website/download.html#ubuilder">http://ueditor.baidu.com/website/download.html#ubuilder</a>
+ - 官方下载：<a target="_blank" href="http://ueditor.baidu.com/website/download.html">http://ueditor.baidu.com/website/download.html</a>
+ - 官方文档：<a target="_blank" href="http://fex.baidu.com/ueditor/">http://fex.baidu.com/ueditor/</a>
+ - 项目地址：<a target="_blank" href="https://github.com/fex-team/ueditor">https://github.com/fex-team/ueditor</a>
+
+### 使用 ZUI 为 UEditor 定制的主题
+
+使用 ZUI 为 UEditor 定制的主题可以更改 UEditor 界面外观与 ZUI 风格保持一致。
+
+你需要将 ZUI `dist/lib/ueditor/ueditor.css` 和 `dist/lib/ueditor/ueditor.min.css` 文件替换在 UEditor 的 `themes/default/css/` 目录下的 CSS 文件。
+
+## Kindeitor
+
+<div class="alert alert-danger">
+  <h4>兼容性问题</h4>
+  <p>在触摸屏或小屏幕上无法获取最佳体验。</p>
+</div>
+
+详细用法请访问 Kindeitor 官方网站：<a target="_blank" href="http://kindeditor.org/">http://kindeditor.org/</a>，项目地址：<a target="_blank" href="hhttps://github.com/kindsoft/kindeditor">hhttps://github.com/kindsoft/kindeditor</a>。
+
+### 使用 Kindeitor
+
+Kindeitor 是作为独立组件，你手动引入 JavaScript 文件到你的页面。
+
+```html
+<script src="dist/lib/kindeditor/kindeditor.min.js"></script>
+```
+
+你不需要额外引入 CSS 文件，因为 Kindeitor 的主题样式文件是通过 JavaScript 加载的。
+
+### 默认模式
+
+<example>
+  <textarea id="content" name="content" class="form-control kindeditor" style="height:150px;">Hello, world!</textarea>
+</example>
+
+```html
+<textarea id="content" name="content" class="form-control kindeditor" style="height:150px;">Hello, world!</textarea>
+```
+
+```javascript
+KindEditor.create('textarea.kindeditor', {
+    basePath: '/dist/lib/kindeditor/',
+    allowFileManager : true,
+    bodyClass : 'article-content'
+});
+```
+
+### 简单模式
+
+自定义工具栏，禁用文件上传。
+
+<example>
+  <textarea id="contentSimple" name="content" class="form-control kindeditorSimple" style="height:150px;">Hello, world!</textarea>
+</example>
+
+```html
+<textarea id="contentSimple" name="content" class="form-control kindeditorSimple" style="height:150px;">Hello, world!</textarea>
+```
+
+```javascript
+KindEditor.create('textarea.kindeditor', {
+    basePath: '/dist/lib/kindeditor/',
+    bodyClass : 'article-content',
+    resizeType : 1,
+    allowPreviewEmoticons : false,
+    allowImageUpload : false,
+    items : [
+        'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic', 'underline',
+        'removeformat', '|', 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist',
+        'insertunorderedlist', '|', 'emoticons', 'image', 'link'
+    ]
+});
+```
+
+<script>
+function onPageLoad() {
+  return false;
+}
+function afterPageLoad() {
+    var initKindeditor = function(){
+        var K = window.KindEditor;
+        if(K) {
+            K.create('textarea.kindeditor', {
+                basePath: '/dist/lib/kindeditor/',
+                allowFileManager : true,
+                bodyClass : 'article-content'
+            });
+
+            K.create('textarea.kindeditorSimple', {
+                basePath: '/dist/lib/kindeditor/',
+                bodyClass : 'article-content',
+                resizeType : 1,
+                allowPreviewEmoticons : false,
+                allowImageUpload : false,
+                items : [
+                    'fontname', 'fontsize', '|', 'forecolor', 'hilitecolor', 'bold', 'italic', 'underline',
+                    'removeformat', '|', 'justifyleft', 'justifycenter', 'justifyright', 'insertorderedlist',
+                    'insertunorderedlist', '|', 'emoticons', 'image', 'link'
+                ]
+            });
+        }
+        setTimeout($.doc.stopPageLoading, 500);
+    };
+
+    $.getScript('/dist/lib/kindeditor/kindeditor.min.js', initKindeditor);
+
+    $(document).on('click', '.ke-dialog-mask, .ke-dialog', function(e){
+        e.stopPropagation();
+    });
+}
+</script>
---
+++ html/nullpointer-2.0/src/js/date.js
@@ -0,0 +1,217 @@
+/* ========================================================================
+ * ZUI: date.js
+ * Date polyfills
+ * http://zui.sexy
+ * ========================================================================
+ * Copyright (c) 2014 cnezsoft.com; Licensed MIT
+ * ======================================================================== */
+
+
+(function() {
+    'use strict';
+
+    /**
+     * Ticks of a whole day
+     * @type {number}
+     */
+    Date.ONEDAY_TICKS = 24 * 3600 * 1000;
+
+    /**
+     * Format date to a string
+     *
+     * @param  string   format
+     * @return string
+     */
+    if(!Date.prototype.format) {
+        Date.prototype.format = function(format) {
+            var date = {
+                'M+': this.getMonth() + 1,
+                'd+': this.getDate(),
+                'h+': this.getHours(),
+                'm+': this.getMinutes(),
+                's+': this.getSeconds(),
+                'q+': Math.floor((this.getMonth() + 3) / 3),
+                'S+': this.getMilliseconds()
+            };
+            if(/(y+)/i.test(format)) {
+                format = format.replace(RegExp.$1, (this.getFullYear() + '').substr(4 - RegExp.$1.length));
+            }
+            for(var k in date) {
+                if(new RegExp('(' + k + ')').test(format)) {
+                    format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? date[k] : ('00' + date[k]).substr(('' + date[k]).length));
+                }
+            }
+            return format;
+        };
+    }
+
+    /**
+     * Add milliseconds to the date
+     * @param {number} value
+     */
+    if(!Date.prototype.addMilliseconds) {
+        Date.prototype.addMilliseconds = function(value) {
+            this.setTime(this.getTime() + value);
+            return this;
+        };
+    }
+
+
+    /**
+     * Add days to the date
+     * @param {number} days
+     */
+    if(!Date.prototype.addDays) {
+        Date.prototype.addDays = function(days) {
+            this.addMilliseconds(days * Date.ONEDAY_TICKS);
+            return this;
+        };
+    }
+
+
+    /**
+     * Clone a new date instane from the date
+     * @return {Date}
+     */
+    if(!Date.prototype.clone) {
+        Date.prototype.clone = function() {
+            var date = new Date();
+            date.setTime(this.getTime());
+            return date;
+        };
+    }
+
+
+    /**
+     * Judge the year is in a leap year
+     * @param  {integer}  year
+     * @return {Boolean}
+     */
+    if(!Date.isLeapYear) {
+        Date.isLeapYear = function(year) {
+            return(((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0));
+        };
+    }
+
+    if(!Date.getDaysInMonth) {
+        /**
+         * Get days number of the date
+         * @param  {integer} year
+         * @param  {integer} month
+         * @return {integer}
+         */
+        Date.getDaysInMonth = function(year, month) {
+            return [31, (Date.isLeapYear(year) ? 29 : 28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month];
+        };
+    }
+
+
+    /**
+     * Judge the date is in a leap year
+     * @return {Boolean}
+     */
+    if(!Date.prototype.isLeapYear) {
+        Date.prototype.isLeapYear = function() {
+            return Date.isLeapYear(this.getFullYear());
+        };
+    }
+
+
+    /**
+     * Clear time part of the date
+     * @return {date}
+     */
+    if(!Date.prototype.clearTime) {
+        Date.prototype.clearTime = function() {
+            this.setHours(0);
+            this.setMinutes(0);
+            this.setSeconds(0);
+            this.setMilliseconds(0);
+            return this;
+        };
+    }
+
+
+    /**
+     * Get days of this month of the date
+     * @return {integer}
+     */
+    if(!Date.prototype.getDaysInMonth) {
+        Date.prototype.getDaysInMonth = function() {
+            return Date.getDaysInMonth(this.getFullYear(), this.getMonth());
+        };
+    }
+
+
+    /**
+     * Add months to the date
+     * @param {date} value
+     */
+    if(!Date.prototype.addMonths) {
+        Date.prototype.addMonths = function(value) {
+            var n = this.getDate();
+            this.setDate(1);
+            this.setMonth(this.getMonth() + value);
+            this.setDate(Math.min(n, this.getDaysInMonth()));
+            return this;
+        };
+    }
+
+
+    /**
+     * Get last week day of the date
+     * @param  {integer} day
+     * @return {date}
+     */
+    if(!Date.prototype.getLastWeekday) {
+        Date.prototype.getLastWeekday = function(day) {
+            day = day || 1;
+
+            var d = this.clone();
+            while(d.getDay() != day) {
+                d.addDays(-1);
+            }
+            d.clearTime();
+            return d;
+        };
+    }
+
+
+    /**
+     * Judge the date is same day as another date
+     * @param  {date}  date
+     * @return {Boolean}
+     */
+    if(!Date.prototype.isSameDay) {
+        Date.prototype.isSameDay = function(date) {
+            return date.toDateString() === this.toDateString();
+        };
+    }
+
+
+    /**
+     * Judge the date is in same week as another date
+     * @param  {date}  date
+     * @return {Boolean}
+     */
+    if(!Date.prototype.isSameWeek) {
+        Date.prototype.isSameWeek = function(date) {
+            var weekStart = this.getLastWeekday();
+            var weekEnd = weekStart.clone().addDays(7);
+            return date >= weekStart && date < weekEnd;
+        };
+    }
+
+
+    /**
+     * Judge the date is in same year as another date
+     * @param  {date}  date
+     * @return {Boolean}
+     */
+    if(!Date.prototype.isSameYear) {
+        Date.prototype.isSameYear = function(date) {
+            return this.getFullYear() === date.getFullYear();
+        };
+    }
+}());
+
