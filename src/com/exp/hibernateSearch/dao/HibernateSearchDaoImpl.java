6786a2c6331312f4df34147d21b8b6cd1816337a
zhangzhaolin <626344962@qq.com>
2016-12-26 14:32:16 +0800

    暂时去除高亮显示

--- src/com/exp/hibernateSearch/dao/HibernateSearchDaoImpl.java
+++ src/com/exp/hibernateSearch/dao/HibernateSearchDaoImpl.java
@@ -132,32 +132,38 @@ public class HibernateSearchDaoImpl extends BaseDao<Bug, String> {
 			list = hibQuery.list();
 
 			// 集关键字高亮的实现代码
-			SimpleHTMLFormatter formatter = new SimpleHTMLFormatter("<font color='red';>", "</font>");
-			QueryScorer queryScorer = new QueryScorer(luceneQuery);
-			Highlighter highlighter = new Highlighter(formatter, queryScorer);
-			Analyzer analyzer = new ChineseAnalyzer();
-			String[] fieldNames = { "bugTitle", "bugDescribe", "bugReason", "bugMethod" };
-			for (Bug q : list) {
-				for (String fieldName : fieldNames) {
-					// 运用反射得到具体的标题内容
-					Object fieldValue = ReflectionUtils
-							.invokeMethod(BeanUtils.getPropertyDescriptor(Bug.class, fieldName).getReadMethod(), q);
-					String hightLightFieldValue = null;
-					if (fieldValue instanceof String) {
-						// 获得高亮关键字
-						System.out.println("bug 高亮" + fieldValue);
-						hightLightFieldValue = highlighter.getBestFragment(analyzer, fieldName,
-								ObjectUtils.toString(fieldValue, null));
-					}
-					// 这个判断很关键，否则如果标题或内容中没有关键字的话，就会出现不显示的问题。
-					if (hightLightFieldValue != null) {
-						// 运用反射设置结果集中的关键字高亮
-						ReflectionUtils.invokeMethod(
-								BeanUtils.getPropertyDescriptor(Bug.class, fieldName).getWriteMethod(), q,
-								hightLightFieldValue);
-					}
-				}
-			}
+			// SimpleHTMLFormatter formatter = new SimpleHTMLFormatter("<font
+			// color='red';>", "</font>");
+			// QueryScorer queryScorer = new QueryScorer(luceneQuery);
+			// Highlighter highlighter = new Highlighter(formatter,
+			// queryScorer);
+			// Analyzer analyzer = new ChineseAnalyzer();
+			// String[] fieldNames = { "bugTitle", "bugDescribe", "bugReason",
+			// "bugMethod" };
+			// for (Bug q : list) {
+			// for (String fieldName : fieldNames) {
+			// // 运用反射得到具体的标题内容
+			// Object fieldValue = ReflectionUtils
+			// .invokeMethod(BeanUtils.getPropertyDescriptor(Bug.class,
+			// fieldName).getReadMethod(), q);
+			// String hightLightFieldValue = null;
+			// if (fieldValue instanceof String) {
+			// // 获得高亮关键字
+			// System.out.println("bug 高亮" + fieldValue);
+			// hightLightFieldValue = highlighter.getBestFragment(analyzer,
+			// fieldName,
+			// ObjectUtils.toString(fieldValue, null));
+			// }
+			// // 这个判断很关键，否则如果标题或内容中没有关键字的话，就会出现不显示的问题。
+			// if (hightLightFieldValue != null) {
+			// // 运用反射设置结果集中的关键字高亮
+			// ReflectionUtils.invokeMethod(
+			// BeanUtils.getPropertyDescriptor(Bug.class,
+			// fieldName).getWriteMethod(), q,
+			// hightLightFieldValue);
+			// }
+			// }
+			// }
 		} catch (Exception e) {
 		}
 		//
@@ -199,41 +205,48 @@ public class HibernateSearchDaoImpl extends BaseDao<Bug, String> {
 		List<Question> list = null;
 		try {
 			// 模糊查询
-			org.apache.lucene.search.Query luceneQuery = qb.keyword()
-					.onFields("questionTitle", "questionDescribe").matching(search).createQuery();
-			//org.apache.lucene.search.Query luceneQuery = qb.keyword().fuzzy().withThreshold(1f)
-			//		.onFields("questionTitle", "questionDescribe").matching(search).createQuery();
+			org.apache.lucene.search.Query luceneQuery = qb.keyword().onFields("questionTitle", "questionDescribe")
+					.matching(search).createQuery();
+			// org.apache.lucene.search.Query luceneQuery =
+			// qb.keyword().fuzzy().withThreshold(1f)
+			// .onFields("questionTitle",
+			// "questionDescribe").matching(search).createQuery();
 			Query hibQuery = fullTextSession.createFullTextQuery(luceneQuery);
 			hibQuery.setFirstResult((pageNum - 1) * pageSize);
 			hibQuery.setMaxResults(pageSize);
 			list = hibQuery.list();
 
 			// 集关键字高亮的实现代码
-			SimpleHTMLFormatter formatter = new SimpleHTMLFormatter("<font color='red';>", "</font>");
-			QueryScorer queryScorer = new QueryScorer(luceneQuery);
-			Highlighter highlighter = new Highlighter(formatter, queryScorer);
-			Analyzer analyzer = new ChineseAnalyzer();
-			String[] fieldNames = { "questionTitle", "questionDescribe" };
-			for (Question q : list) {
-				for (String fieldName : fieldNames) {
-					// 运用反射得到具体的标题内容
-					Object fieldValue = ReflectionUtils.invokeMethod(
-							BeanUtils.getPropertyDescriptor(Question.class, fieldName).getReadMethod(), q);
-					String hightLightFieldValue = null;
-					if (fieldValue instanceof String) {
-						// 获得高亮关键字
-						hightLightFieldValue = highlighter.getBestFragment(analyzer, fieldName,
-								ObjectUtils.toString(fieldValue, null));
-					}
-					// 这个判断很关键，否则如果标题或内容中没有关键字的话，就会出现不显示的问题。
-					if (hightLightFieldValue != null) {
-						// 运用反射设置结果集中的关键字高亮
-						ReflectionUtils.invokeMethod(
-								BeanUtils.getPropertyDescriptor(Question.class, fieldName).getWriteMethod(), q,
-								hightLightFieldValue);
-					}
-				}
-			}
+			// SimpleHTMLFormatter formatter = new SimpleHTMLFormatter("<font
+			// color='red';>", "</font>");
+			// QueryScorer queryScorer = new QueryScorer(luceneQuery);
+			// Highlighter highlighter = new Highlighter(formatter,
+			// queryScorer);
+			// Analyzer analyzer = new ChineseAnalyzer();
+			// String[] fieldNames = { "questionTitle", "questionDescribe" };
+			// for (Question q : list) {
+			// for (String fieldName : fieldNames) {
+			// // 运用反射得到具体的标题内容
+			// Object fieldValue = ReflectionUtils.invokeMethod(
+			// BeanUtils.getPropertyDescriptor(Question.class,
+			// fieldName).getReadMethod(), q);
+			// String hightLightFieldValue = null;
+			// if (fieldValue instanceof String) {
+			// // 获得高亮关键字
+			// hightLightFieldValue = highlighter.getBestFragment(analyzer,
+			// fieldName,
+			// ObjectUtils.toString(fieldValue, null));
+			// }
+			// // 这个判断很关键，否则如果标题或内容中没有关键字的话，就会出现不显示的问题。
+			// if (hightLightFieldValue != null) {
+			// // 运用反射设置结果集中的关键字高亮
+			// ReflectionUtils.invokeMethod(
+			// BeanUtils.getPropertyDescriptor(Question.class,
+			// fieldName).getWriteMethod(), q,
+			// hightLightFieldValue);
+			// }
+			// }
+			// }
 		} catch (Exception e) {
 		}
 		//
